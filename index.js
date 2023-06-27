8cab80b0c8007ade34cd8ee1454fd907685909c3
JonyPlo <joonyyplo@gmail.com>
2023-06-27 10:14:32 -0300

    Se realizo la conexion a la base de datos

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import path from 'path';
+import 'dotenv/config'
+import './src/database/dbConnectio'
 
 //Tomar un puerto
 //crear una instancia de express
