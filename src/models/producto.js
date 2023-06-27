645182694bb3d6555c8b1a8d19d89c811b0d7b23
JonyPlo <joonyyplo@gmail.com>
2023-06-27 10:33:48 -0300

    Se creo el modelo para los productos

---
+++ src/models/producto.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    minLength: 2,
+    maxLength: 100,
+    required: true,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    min: 1,
+    max: 10000,
+    required: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+});
+
+const Producto = model('producto', productoSchema);
+
+export default Producto;
