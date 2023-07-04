a47fa0e9894f483c17f8052b7001c70bb7e83307
Emilse Arias <emilsearias@gmail.com>
2023-07-04 10:41:42 -0300

    agregamos las validaciones de los campos de imagen y categoria

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -17,7 +17,37 @@ const router = Router();
 router
   .route("/productos")
   .get(obtenerProductos)
-  .post([check("nombreProducto").notEmpty().withMessage('El nombre del producto es obligatorio')], crearProducto);
+  .post(
+    [
+      check("nombreProducto")
+        .notEmpty()
+        .withMessage("El nombre del producto es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+          "El nombre del producto debe contener entre 2 y 100 caracteres"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio es un dato obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe tener un formato numerico")
+        .custom((value) => {
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            throw new Error("El precio debe ser entre 1 y 10000");
+          }
+        }),
+      check("imagen")
+        .notEmpty()
+        .withMessage("La dirección de la imagen es obligatoria")
+        .matches(
+          /^(https?:\/\/)?(?:www\.)?[\w-]+\.[\w.-]+(?:\/[\w-./?%&=]*)?\.(?:jpg|jpeg|png|gif|bmp|jpeg\?[\w=&.]*)$/
+        )
+        .withMessage("Debe ingresar una url de imagen valida"),
+    ],
+    crearProducto
+  );
 router
   .route("/productos/:id")
   .delete(borrarProducto)
