42b9fbb3716956a1afd4b5153e2afc23c1c4bf6c
Emilse Arias <emilsearias@gmail.com>
2023-07-06 10:28:36 -0300

    agregue cambios en el modelo de usuario

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,37 +1,40 @@
-import { Schema,model } from "mongoose";
+import { Schema, model } from "mongoose";
 
 const usuarioSchema = new Schema({
-    nombreUsuario:{
-    type:String,
-    required:true,
-    minLength:2,
-    maxLength:16,
-    unique:true
+  nombreUsuario: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 16,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 16,
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,16}$/.test(
+          value
+        );
+      },
+      message:
+        "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un carácter especial y un número. Además, la longitud máxima es de 16 caracteres.",
     },
-    password:{
-    type:String,
-    required:true,
-    minLength:8,
-    maxLength:16,
-    validate:{
-        validator:function (value) {
-            return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,16}$/.test(value)
-        },
-        message: 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un carácter especial y un número. Además, la longitud máxima es de 16 caracteres.'
-    }
-    },
-    email:{
-    type:String,
+  },
+  email: {
+    type: String,
     required: true,
     unique: true,
     validate: {
-      validator: function(value) {
+      validator: function (value) {
         return /\S+@\S+\.\S+/.test(value);
       },
-      message: 'El email no es válido.'
-    }
-    }
-})
+      message: "El email no es válido.",
+    },
+  },
+});
 
-const Usuario = model("Usuario",usuarioSchema)
+const Usuario = model("Usuario", usuarioSchema);
 export default Usuario;
